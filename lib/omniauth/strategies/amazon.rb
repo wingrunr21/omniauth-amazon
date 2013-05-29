4bd6e7c673ff86d65eb30795b9455a648b1fe1c4
Stafford Brunk <wingrunr21@gmail.com>
2013-05-29 16:28:40 -0600

    Fix the raw_info profile lookup

--- lib/omniauth/strategies/amazon.rb
+++ lib/omniauth/strategies/amazon.rb
@@ -4,6 +4,8 @@ require 'multi_json'
 module OmniAuth
   module Strategies
     class Amazon < OmniAuth::Strategies::OAuth2
+      option :name, 'amazon'
+
       option :client_options, {
         :site => 'https://www.amazon.com/',
         :authorize_url => 'https://www.amazon.com/ap/oa',
@@ -11,17 +13,12 @@ module OmniAuth
       }
 
       option :access_token_options, {
-        :mode => :query,
-        :param_name => 'access_token'
+        :mode => :query
       }
 
       option :authorize_params, {
         :scope => 'profile postal_code'
       }
-
-      def request_phase
-        super
-      end
 
       def build_access_token
         token_params = {
@@ -33,25 +30,33 @@ module OmniAuth
         client.auth_code.get_token(verifier, token_params)
       end
 
-      uid { raw_info.parsed['user_id'] }
+      uid { raw_info['Profile']['CustomerId'] }
 
       info do
         {
-          'email' => raw_info.parsed['email'],
-          'name' => raw_info.parsed['name']
+          'email' => raw_info['Profile']['PrimaryEmail'],
+          'name' => raw_info['Profile']['Name']
         }
       end
 
       extra do
         {
-          'postal_code' => raw_info.parsed['postal_code']
+          'postal_code' => raw_info['Profile']['PostalCode']
         }
       end
 
       def raw_info
-        @raw_info ||= access_token.get('/ap/user/profile').parsed
-      end
+        access_token.options[:parse] = :json
 
+        # This way is not working right now, do it the longer way
+        # for the time being
+        #
+        #@raw_info ||= access_token.get('/ap/user/profile').parsed
+
+        url = "/ap/user/profile"
+        params = {params:{access_token: access_token.token}}
+        @raw_info ||= access_token.client.request(:get, url, params).parsed
+      end
     end
   end
 end
